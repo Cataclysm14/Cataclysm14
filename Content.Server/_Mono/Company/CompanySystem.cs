fce8e017d8487b867168460a0f41fa24fcbb73d8
BlueHNT <79374236+BlueHNT@users.noreply.github.com>
2025-06-11 19:25:24 +0200

    adds Emergency Responder, updates DoC requirements

--- Content.Server/_Mono/Company/CompanySystem.cs
+++ Content.Server/_Mono/Company/CompanySystem.cs
@@ -35,6 +35,7 @@ public sealed class CompanySystem : EntitySystem
         "NFDetective",
         "PublicAffairsLiaison",
         "DirectorOfCare",
+        "MdMedic",
         "SecurityGuard",
         "Cadet"
     };
