23338f302a90c212691f69e80ac1e51bc82c7439
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-06-30 04:09:14 +0000

    chore: automatically update REUSE headers

--- Content.Goobstation.Client/Clothing/Components/SealableClothingVisualsComponent.cs
+++ Content.Goobstation.Client/Clothing/Components/SealableClothingVisualsComponent.cs
@@ -1,12 +1,11 @@
-// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 Aiden <aiden@djkraz.com>
-// SPDX-FileCopyrightText: 2025 BombasterDS <115770678+BombasterDS@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 BombasterDS <deniskaporoshok@gmail.com>
-// SPDX-FileCopyrightText: 2025 BombasterDS2 <shvalovdenis.workmail@gmail.com>
-// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
-// SPDX-FileCopyrightText: 2025 Marty <martynashagriefer@gmail.com>
-// SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
-// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
+// SPDX-FileCopyrightText: 2025 Aiden
+// SPDX-FileCopyrightText: 2025 BombasterDS
+// SPDX-FileCopyrightText: 2025 BombasterDS2
+// SPDX-FileCopyrightText: 2025 GoobBot
+// SPDX-FileCopyrightText: 2025 Marty
+// SPDX-FileCopyrightText: 2025 Misandry
+// SPDX-FileCopyrightText: 2025 NotActuallyMarty
+// SPDX-FileCopyrightText: 2025 gus
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
