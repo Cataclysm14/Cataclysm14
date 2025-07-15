62a812aabec3a8cdc8399355c514e6de7e9e30ea
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-07-15 17:54:24 +0000

    chore: automatically update REUSE headers

--- Content.Server/_Mono/Research/PointDiskPrinter/Components/PointDiskConsolePrintingComponent.cs
+++ Content.Server/_Mono/Research/PointDiskPrinter/Components/PointDiskConsolePrintingComponent.cs
@@ -1,4 +1,10 @@
-﻿namespace Content.Server._Mono.Research.PointDiskPrinter.Components;
+// SPDX-FileCopyrightText: 2022 Nemanja
+// SPDX-FileCopyrightText: 2023 DrSmugleaf
+// SPDX-FileCopyrightText: 2025 Onezero0
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Server._Mono.Research.PointDiskPrinter.Components;
 
 [RegisterComponent]
 public sealed partial class PointDiskConsolePrintingComponent : Component
