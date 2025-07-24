e3a404e45de9e67b8849dcc2121c1438ac5ae853
Redrover1760 <39284090+Redrover1760@users.noreply.github.com>
2025-07-24 13:32:18 -0400

    works now

--- Content.Server/Shuttles/Systems/GridCleanupSystem.cs
+++ Content.Server/Shuttles/Systems/GridCleanupSystem.cs
@@ -181,6 +181,7 @@ public sealed class GridCleanupSystem : EntitySystem
 
             // Check to ensure no mobs will be deleted
             var mobQuery = AllEntityQuery<MobStateComponent, TransformComponent>();
+            var entityCheck = false;
             while (mobQuery.MoveNext(out var mobUid, out _, out var mobxform))
             {
                 if (mobxform.GridUid == null || mobxform.MapUid == null || mobxform.GridUid != xform.GridUid)
@@ -188,12 +189,17 @@ public sealed class GridCleanupSystem : EntitySystem
 
                 Logger.DebugS("salvage", $"Update: Mob {mobUid} detected on {gridUid}, removing grid from cleanup queue");
                 toRemove.Add(gridUid);
+                entityCheck = true;
+                break;
             }
 
+            if (entityCheck == true)
+                continue;
+
             // Delete the grid immediately to prevent the possibility of a mob entering after deletion is queued
+            Del(gridUid);
             Logger.DebugS("salvage", $"Update: Deleting {gridUid} with {CountTiles((gridUid, grid))} tiles");
             toRemove.Add(gridUid);
-            Del(gridUid);
         }
 
         // Remove processed grids from the pending list
