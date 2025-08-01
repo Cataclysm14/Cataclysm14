64e820599fb5e0c187e6f691ff3585f1cfba9588
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-08-01 02:04:23 +0000

    chore: automatically update REUSE headers

--- Content.Shared/CCVar/CCVars.Atmos.cs
+++ Content.Shared/CCVar/CCVars.Atmos.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Simon
 // SPDX-FileCopyrightText: 2025 GreaseMonk
 // SPDX-FileCopyrightText: 2025 sleepyyapril
+// SPDX-FileCopyrightText: 2025 tonotom
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
