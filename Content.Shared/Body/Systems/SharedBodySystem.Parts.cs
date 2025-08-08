1c3e86e83b593adeb56c33cdc5851c43861215eb
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-08-08 07:10:39 +0000

    chore: automatically update REUSE headers

--- Content.Shared/Body/Systems/SharedBodySystem.Parts.cs
+++ Content.Shared/Body/Systems/SharedBodySystem.Parts.cs
@@ -8,6 +8,7 @@
 // SPDX-FileCopyrightText: 2024 0x6273
 // SPDX-FileCopyrightText: 2024 slarticodefast
 // SPDX-FileCopyrightText: 2025 Ark
+// SPDX-FileCopyrightText: 2025 Coenx-flex
 // SPDX-FileCopyrightText: 2025 Redrover1760
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
