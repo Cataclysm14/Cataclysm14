bbe5e2569262101c775228c50acf7b6b5042f249
Coenx-flex <coengmurray@gmail.com>
2025-08-08 19:26:03 -0500

    Fix Linter error

--- Content.Shared/_Mono/CorticalBorer/Components/CorticalBorerComponent.cs
+++ Content.Shared/_Mono/CorticalBorer/Components/CorticalBorerComponent.cs
@@ -21,18 +21,21 @@ public sealed partial class CorticalBorerComponent : Component
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
     [AutoNetworkedField]
+    [DataField]
     public int ChemicalPoints = 50;
 
     /// <summary>
     /// Chemicals added every second WHILE IN A HOST
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public int ChemicalGenerationRate = 1;
 
     /// <summary>
     /// Max Chemicals that can be held
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public int ChemicalPointCap = 250;
 
     /// <summary>
@@ -44,6 +47,7 @@ public sealed partial class CorticalBorerComponent : Component
     /// The max duration you can take control of your host
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public TimeSpan ControlDuration = TimeSpan.FromSeconds(40);
 
     /// <summary>
@@ -56,18 +60,21 @@ public sealed partial class CorticalBorerComponent : Component
     /// Can this borer make more
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public bool CanReproduce = true;
 
     /// <summary>
     /// What does it vomit out of its mouth when it lays an egg
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public string EggProto = "CorticalBorerEgg";
 
     /// <summary>
     /// cost to lay an egg... will not update ability desc if changed
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public int EggCost = 200;
 
     public readonly List<EntProtoId> InitialCorticalBorerActions = new()
