1668cae82ea28931ad4c4d52e78c194f369434bb
Redrover1760 <39284090+Redrover1760@users.noreply.github.com>
2025-08-11 21:12:03 -0400

    try try again

--- Content.Server/Spawners/EntitySystems/SpawnPointSystem.cs
+++ Content.Server/Spawners/EntitySystems/SpawnPointSystem.cs
@@ -102,10 +102,10 @@ public sealed class SpawnPointSystem : EntitySystem
             }
             else
             {
-                if (TryComp<MetaDataComponent>(args.Station, out var data) && data.EntityName != null)
-                    Log.Error($"No spawn points were available for station {data.EntityName}! (uid: {args.Station})");
+                if (TryComp<MetaDataComponent>(args.Station, out var data) && data.EntityPrototype != null)
+                    Log.Error($"No spawn points were available for station {data.EntityPrototype}! (uid: {args.Station})");
                 else
-                    Log.Error($"No spawn points were available for station {args.Station}!, station name is null.");
+                    Log.Error($"No spawn points were available for station {args.Station}!, station prototype is null.");
                 return;
             }
         }
