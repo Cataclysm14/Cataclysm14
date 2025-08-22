7223bc4abf118e11cab241cdecd1c53e9f051109
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-08-22 07:44:47 +0000

    chore: automatically update REUSE headers

--- Content.Server/NPC/Systems/NPCCombatSystem.Ranged.cs
+++ Content.Server/NPC/Systems/NPCCombatSystem.Ranged.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2024 Dvir
 // SPDX-FileCopyrightText: 2025 Ark
 // SPDX-FileCopyrightText: 2025 Aviu00
+// SPDX-FileCopyrightText: 2025 Ilya246
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
