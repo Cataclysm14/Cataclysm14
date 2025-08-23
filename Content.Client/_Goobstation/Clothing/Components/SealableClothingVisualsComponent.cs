12fbac4e20ca2d802b9b53f13130ed45294a27f3
core-mene <fluorine2@live.fr>
2025-08-23 22:10:56 +0200

    maybe

--- Content.Client/_Goobstation/Clothing/Components/SealableClothingVisualsComponent.cs
+++ Content.Client/_Goobstation/Clothing/Components/SealableClothingVisualsComponent.cs
@@ -1,4 +1,13 @@
+// SPDX-FileCopyrightText: 2025 Aiden
+// SPDX-FileCopyrightText: 2025 BombasterDS
+// SPDX-FileCopyrightText: 2025 BombasterDS2
+// SPDX-FileCopyrightText: 2025 GoobBot
+// SPDX-FileCopyrightText: 2025 Marty
+// SPDX-FileCopyrightText: 2025 Misandry
+// SPDX-FileCopyrightText: 2025 NotActuallyMarty
 // SPDX-FileCopyrightText: 2025 Redrover1760
+// SPDX-FileCopyrightText: 2025 core-mene
+// SPDX-FileCopyrightText: 2025 gus
 // SPDX-FileCopyrightText: 2025 starch
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
