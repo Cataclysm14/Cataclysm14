fb5f1b0aa05c41811e356432079aed7c75b928c8
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-08-23 23:27:02 +0000

    chore: automatically update REUSE headers

--- Content.IntegrationTests/Tests/Station/EvacShuttleTest.cs
+++ Content.IntegrationTests/Tests/Station/EvacShuttleTest.cs
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2024 DrSmugleaf
+// SPDX-FileCopyrightText: 2024 Leon Friedrich
+// SPDX-FileCopyrightText: 2025 Coenx-flex
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Linq;
 using Content.Server.GameTicking;
 using Content.Server.Shuttles.Components;
