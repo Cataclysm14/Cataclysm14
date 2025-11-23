b5b4a36187b271a11005ca22322f3101961bf4ab
tonotom1 <tonotom@proton.me>
2025-11-23 05:10:24 -0800

    Update ReagentGrinderSystem.cs

--- Content.Server/Kitchen/EntitySystems/ReagentGrinderSystem.cs
+++ Content.Server/Kitchen/EntitySystems/ReagentGrinderSystem.cs
@@ -29,11 +29,10 @@
 // SPDX-FileCopyrightText: 2024 Whatstone
 // SPDX-FileCopyrightText: 2024 beck-thompson
 // SPDX-FileCopyrightText: 2024 checkraze
+// SPDX-FileCopyrightText: 2025 imatsoup
 // SPDX-FileCopyrightText: 2025 tonotom1
 //
 // SPDX-License-Identifier: MPL-2.0
-
-// SPDX-FileCopyrightText: 2025 imatsoup
 
 using Content.Server.Chemistry.Containers.EntitySystems; // Frontier
 using Content.Server.Construction; // Frontier
