8afe1d9cf232ff57f970dbdb64b53c180e85a7c3
core-mene <fluorine2@live.fr>
2025-12-11 02:05:12 +0100

    Update HumanInventoryUniformSlotsTest.cs

--- Content.IntegrationTests/Tests/HumanInventoryUniformSlotsTest.cs
+++ Content.IntegrationTests/Tests/HumanInventoryUniformSlotsTest.cs
@@ -86,7 +86,7 @@ namespace Content.IntegrationTests.Tests
 
                     // Can't equip any of these since no uniform!
                     Assert.That(invSystem.CanEquip(human, idCard, "id", out _), Is.True);  // Cataclysm, Is.False -> Is.True, nuked pocket/ID requirement
-                    Assert.That(invSystem.CanEquip(human, pocketItem, "pocket1", out _), Is.True); // Cataclysm, same as above
+                    Assert.That(invSystem.CanEquip(human, pocketItem, "pocket1", out _), Is.False);
                     Assert.That(invSystem.CanEquip(human, tooBigItem, "pocket2", out _), Is.False); // This one fails either way.
                 });
 
