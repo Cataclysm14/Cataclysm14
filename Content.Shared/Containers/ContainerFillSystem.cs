b060cfb80b83b60566b0c506211f3883f79ebc79
BadRyuner <54708336+BadRyuner@users.noreply.github.com>
2026-07-31 14:12:35 +0300

    add clothing shop

--- Content.Shared/Containers/ContainerFillSystem.cs
+++ Content.Shared/Containers/ContainerFillSystem.cs
@@ -1,9 +1,13 @@
 using System.Numerics;
-using Content.Shared._Cataclysm14.Containers;
 using Content.Shared.EntityTable;
 using Robust.Shared.Containers;
 using Robust.Shared.Map;
 using Robust.Shared.Random;
+
+using Content.Shared._Cataclysm14.Containers; // Cataclysm14
+using Content.Shared._Cataclysm14.Storage; // Cataclysm14
+using Content.Shared.Item; // Cataclysm14
+using Content.Shared.Storage; // Cataclysm14
 
 namespace Content.Shared.Containers;
 
@@ -13,13 +17,14 @@ public sealed partial class ContainerFillSystem : EntitySystem
     [Dependency] private readonly EntityTableSystem _entityTable = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly SharedItemSystem _item = default!; // Cataclysm14
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<ContainerFillComponent, MapInitEvent>(OnMapInit);
         SubscribeLocalEvent<EntityTableContainerFillComponent, MapInitEvent>(OnTableMapInit);
-        SubscribeLocalEvent<EntityTableOutfitContainerFillComponent, MapInitEvent>(OnOutfitMapInit);
+        SubscribeLocalEvent<EntityTableOutfitContainerFillComponent, MapInitEvent>(OnOutfitMapInit); // Cataclysm14
     }
 
     private void OnMapInit(EntityUid uid, ContainerFillComponent component, MapInitEvent args)
@@ -51,6 +56,7 @@ public sealed partial class ContainerFillSystem : EntitySystem
         }
     }
 
+    // Cataclysm14 Begin
     private void OnTableMapInit(Entity<EntityTableContainerFillComponent> ent, ref MapInitEvent args)
     {
         if (!TryComp(ent, out ContainerManagerComponent? containerComp))
@@ -76,6 +82,17 @@ public sealed partial class ContainerFillSystem : EntitySystem
                 var spawn = Spawn(proto, coords);
                 if (!_containerSystem.Insert(spawn, container, containerXform: xform))
                 {
+                    if (HasComp<ResizableStorageOnFillComponent>(ent.Owner)
+                        && TryComp(ent.Owner, out StorageComponent? storageComponent)
+                        && TryComp(spawn, out ItemComponent? itemComponent))
+                    {
+                        storageComponent.Grid[0] = _item.GetAdjustedItemShape((spawn, itemComponent), Angle.Zero, Vector2i.Zero)[0];
+                        Dirty(ent, storageComponent);
+                        if (_containerSystem.Insert(spawn, container, containerXform: xform))
+                            Log.Error($"Entity {ToPrettyString(ent)} with a {nameof(EntityTableContainerFillComponent)} and {nameof(ResizableStorageOnFillComponent)} failed to insert an entity: {ToPrettyString(spawn)}.");
+                        break;
+                    }
+
                     Log.Error($"Entity {ToPrettyString(ent)} with a {nameof(EntityTableContainerFillComponent)} failed to insert an entity: {ToPrettyString(spawn)}.");
                     _transform.AttachToGridOrMap(spawn);
                     break;
@@ -83,4 +100,5 @@ public sealed partial class ContainerFillSystem : EntitySystem
             }
         }
     }
+    // Cataclysm14 End
 }
---
+++ Content.Shared/_Cataclysm14/Storage/ResizableStorageOnFillComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace Content.Shared._Cataclysm14.Storage;
+
+[RegisterComponent]
+public sealed partial class ResizableStorageOnFillComponent : Component
+{
+
+}
