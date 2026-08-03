950c0646f84e3bf48df9575df22641a47f635b4c
BadRyuner <54708336+BadRyuner@users.noreply.github.com>
2026-08-03 10:11:53 +0300

    revert pulpablesystem

--- Content.Server/Destructible/Thresholds/Behaviors/GibBehavior.cs
+++ Content.Server/Destructible/Thresholds/Behaviors/GibBehavior.cs
@@ -11,21 +11,13 @@ namespace Content.Server.Destructible.Thresholds.Behaviors
     {
         [DataField] public GibType GibType = GibType.Gib; // Shitmed Change
         [DataField] public GibContentsOption GibContents = GibContentsOption.Drop; // Shitmed Change
-        [DataField] public bool DestroyOrgans = false;
         [DataField("recursive")] private bool _recursive = true;
 
         public void Execute(EntityUid owner, DestructibleSystem system, EntityUid? cause = null)
         {
             if (system.EntityManager.TryGetComponent(owner, out BodyComponent? body))
             {
-                // Cataclysm14 Begin
-                var gibbedParts = system.BodySystem.GibBody(owner, _recursive, body, gib: GibType, contents: GibContents); // Shitmed Change
-                foreach (var gibbedPart in gibbedParts)
-                {
-                    if (system.EntityManager.TryGetComponent(gibbedPart, out OrganComponent? organ))
-                        system.EntityManager.DeleteEntity(gibbedPart);
-                }
-                // Cataclysm14 End
+                system.BodySystem.GibBody(owner, _recursive, body, gib: GibType, contents: GibContents); // Shitmed Change
             }
         }
     }
