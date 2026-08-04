cd8e55ab692dd0d93ee9709d486058fc21de279b
BadRyuner <54708336+BadRyuner@users.noreply.github.com>
2026-08-04 17:54:26 +0300

    add smoker zombie and zombie hulk

---
+++ Content.Server/_Cataclysm14/Triggers/RepeatTimerTriggerComponent.cs
@@ -0,0 +1,12 @@
+﻿using Content.Shared.FixedPoint;
+
+namespace Content.Server._Cataclysm14.Triggers;
+
+[RegisterComponent]
+public sealed partial class RepeatTimerTriggerComponent : Component
+{
+    [DataField]
+    public FixedPoint2 Interval = 1f;
+
+    public FixedPoint2 CurrentTime;
+}
---
+++ Content.Server/_Cataclysm14/Triggers/TriggerSystem.cs
@@ -0,0 +1,24 @@
+﻿using Content.Shared.FixedPoint;
+
+namespace Content.Server._Cataclysm14.Triggers;
+
+public sealed class TriggerSystem : EntitySystem
+{
+    [Dependency] private Explosion.EntitySystems.TriggerSystem _triggerSystem = default!;
+
+    public override void Update(float frameTime)
+    {
+        var time = FixedPoint2.New(frameTime);
+
+        var timers = EntityQueryEnumerator<RepeatTimerTriggerComponent>();
+        while (timers.MoveNext(out var uid, out var comp))
+        {
+            comp.CurrentTime += time;
+            if (comp.CurrentTime >= comp.Interval)
+            {
+                comp.CurrentTime -= comp.Interval;
+                _triggerSystem.Trigger(uid);
+            }
+        }
+    }
+}
